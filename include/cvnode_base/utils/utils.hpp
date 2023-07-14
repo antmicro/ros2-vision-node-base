357f11ee4deb878b6936856b39a5b10e7652d177
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-07-14 11:01:51 +0200

    [#44883] utils: Add Image message to cv::Mat conversion function

---
+++ include/cvnode_base/utils/utils.hpp
@@ -0,0 +1,23 @@
+// Copyright 2022-2023 Antmicro <www.antmicro.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include <sensor_msgs/msg/image.h>
+
+namespace cvnode_base
+{
+
+/**
+ * Convert a sensor_msgs::Image to a cv::Mat with a given encoding.
+ *
+ * @param img The sensor_msgs::Image to convert.
+ * @param encoding The encoding to use for the cv::Mat.
+ *
+ * @return Converted cv::Mat.
+ */
+cv::Mat imageToMat(const sensor_msgs::msg::Image::SharedPtr &img, const std::string &encoding);
+
+} // namespace cvnode_base
---
+++ src/utils/utils.cpp
@@ -0,0 +1,43 @@
+// Copyright 2022-2023 Antmicro <www.antmicro.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <cv_bridge/cv_bridge.h>
+#include <cvnode_base/utils/utils.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+namespace cvnode_base
+{
+
+cv::Mat imageToMat(const sensor_msgs::msg::Image::SharedPtr &img, const std::string &encoding)
+{
+    cv_bridge::CvImagePtr cv_ptr;
+    if (img->encoding == sensor_msgs::image_encodings::TYPE_8UC3)
+    {
+        cv_ptr = cv_bridge::toCvCopy(img, img->encoding);
+        cv_ptr->encoding = sensor_msgs::image_encodings::BGR8;
+    }
+    else if (img->encoding == sensor_msgs::image_encodings::TYPE_8UC4)
+    {
+        cv_ptr = cv_bridge::toCvCopy(img, img->encoding);
+        cv_ptr->encoding = sensor_msgs::image_encodings::BGRA8;
+    }
+
+    if (img->encoding == encoding)
+    {
+        return cv_ptr->image;
+    }
+
+    try
+    {
+        cv_ptr = cv_bridge::cvtColor(cv_ptr, encoding);
+    }
+    catch (cv_bridge::Exception &e)
+    {
+        RCLCPP_ERROR(rclcpp::get_logger("cvnode_base"), "cv_bridge exception: %s", e.what());
+        return cv::Mat();
+    }
+    return cv_ptr->image;
+}
+
+} // namespace cvnode_base
