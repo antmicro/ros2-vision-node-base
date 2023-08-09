1caf73c8a86cf5180e3efd8c6d030cee65935c16
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-08-09 16:11:27 +0200

    [#44370] examples: mask_rcnn: mask_rcnn_torchscript_launch.py: Add launch script
    Moves launch script and JSON runtime configuration to 'examples/mask_rcnn' folder.
    
    Adds 'CVNodeManager' and 'Kenning' to launch script in order to execute
    full flow.

--- examples/mask_rcnn/mask_rcnn_torchscript_launch.py
+++ examples/mask_rcnn/mask_rcnn_torchscript_launch.py
@@ -6,7 +6,7 @@ from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, Shutdown
 from launch.substitutions import LaunchConfiguration
 
 
@@ -17,6 +17,7 @@ def generate_launch_description():
         default_value='',
         description='Path to the TorchScript model file'
     )
+
     mask_rcnn_node_container = ComposableNodeContainer(
         name='mask_rcnn_node_container',
         namespace='',
@@ -34,7 +35,30 @@ def generate_launch_description():
         ],
         output='both',
     )
+
+    cvnode_manager_node = ComposableNodeContainer(
+        name='cvnode_manager_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='cvnode_manager',
+                plugin='cvnode_manager::CVNodeManager',
+                name='sample_cvnode_manager')
+        ],
+        output='both',
+        on_exit=Shutdown()
+    )
+
+    kenning_node = ExecuteProcess(
+        name='kenning_node',
+        cmd='python -m kenning test --json-cfg ./src/cvnode_base/examples/mask_rcnn/mask_rcnn_ros2_inference.json --measurements ./build/ros2-client-measurements.json --verbosity INFO'.split(' ')  # noqa: E501
+    )
+
     return LaunchDescription([
         model_path,
         mask_rcnn_node_container,
+        cvnode_manager_node,
+        kenning_node,
     ])
