722aaeeda52cadcfcd5abc245e827151ef838bd5
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-08-18 18:15:06 +0200

    [#44370] examples: mask_rcnn: mask_rcnn_detectron_launch.py: Add 'log_level' parameter

--- examples/mask_rcnn/mask_rcnn_detectron_launch.py
+++ examples/mask_rcnn/mask_rcnn_detectron_launch.py
@@ -6,15 +6,25 @@ from launch_ros.actions import ComposableNodeContainer, Node
 from launch_ros.descriptions import ComposableNode
 
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, Shutdown
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, Shutdown
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
     """Launches MaskRCNN node implemented with Detectron2."""
 
+    log_level = DeclareLaunchArgument(
+        'log_level',
+        default_value='INFO',
+        description='Log level'
+    )
+
     mask_rcnn_node = Node(
             package='cvnode_base',
-            executable='mask_rcnn_detectron.py'
+            executable='mask_rcnn_detectron.py',
+            name='mask_rcnn_node',
+            arguments=['--ros-args',
+                       '--log-level', LaunchConfiguration('log_level')]
     )
 
     cvnode_manager_node = ComposableNodeContainer(
@@ -29,15 +39,21 @@ def generate_launch_description():
                 name='sample_cvnode_manager')
         ],
         output='both',
+        arguments=['--ros-args',
+                   '--log-level', LaunchConfiguration('log_level')],
         on_exit=Shutdown()
     )
 
     kenning_node = ExecuteProcess(
         name='kenning_node',
-        cmd='python -m kenning test --json-cfg ./src/cvnode_base/examples/mask_rcnn/mask_rcnn_ros2_inference.json --measurements ./build/ros2-client-measurements.json --verbosity INFO'.split(' ')  # noqa: E501
+        cmd='python -m kenning test \
+--json-cfg ./src/cvnode_base/examples/mask_rcnn/mask_rcnn_ros2_inference.json \
+--measurements ./build/ros2-client-measurements.json \
+--verbosity INFO'.split(' ')
     )
 
     return LaunchDescription([
+        log_level,
         mask_rcnn_node,
         cvnode_manager_node,
         kenning_node,
