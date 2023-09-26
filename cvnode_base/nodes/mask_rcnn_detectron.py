a55958643f69a454c43e117f50394ffcbb911bc8
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-09-26 16:55:12 +0200

    [#49635] nodes: mask_rcnn_detectron.py: Add execution
    Moves execution functionality into a file with node declaration
    
    Signed-off-by: Illia Vysochyn <ivysochyn@internships.antmicro.com>

--- cvnode_base/nodes/mask_rcnn_detectron.py
+++ cvnode_base/nodes/mask_rcnn_detectron.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Copyright 2022-2023 Antmicro <www.antmicro.com>
 #
 # SPDX-License-Identifier: Apache-2.0
@@ -180,3 +182,15 @@ class MaskRCNNDetectronNode(BaseCVNode):
         del self.model
         del self.aug
         collect()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MaskRCNNDetectronNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
