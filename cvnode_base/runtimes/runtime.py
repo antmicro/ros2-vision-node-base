689c016defd4b5c11a7738c1735147c063fdf870
Illia Vysochyn <ivysochyn@antmicro.com>
2023-12-08 18:56:43 +0100

    [#50099] runtimes: runtime.py: Add Kenning runtime compatible with project
    Signed-off-by: Illia Vysochyn <ivysochyn@antmicro.com>

---
+++ cvnode_base/runtimes/runtime.py
@@ -0,0 +1,29 @@
+# Copyright 2022-2023 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""Runtime to use in Kenning scenarios for CVNode"""
+
+from kenning.core.runtime import Runtime
+
+
+class CVNodeRuntime(Runtime):
+    """
+    Represents the runtime for CVNode scenarios.
+    Utilizes the default implementation of the Runtime class.
+
+    Any unused methods are not implemented,
+    ensuring that abstract method errors are not raised.
+    """
+
+    def extract_output(self):
+        raise NotImplementedError
+
+    def prepare_model(self, input_data):
+        raise NotImplementedError
+
+    def run(self):
+        raise NotImplementedError
+
+    def load_input(self, X):
+        raise NotImplementedError
