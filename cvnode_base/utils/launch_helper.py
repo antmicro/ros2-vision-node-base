b2a4e1f17a4ea26c8feee06aefb8fdf2b0a389cf
Illia Vysochyn <ivysochyn@antmicro.com>
2024-01-12 19:09:23 +0100

    [#53508] cvnode_base: utils: launch_helper.py: Add functions for launch scripts
    Defines functions for frequently encountered and repeted parameters/nodes declarations.
    
    Signed-off-by: Illia Vysochyn <ivysochyn@antmicro.com>

---
+++ cvnode_base/utils/launch_helper.py
@@ -0,0 +1,293 @@
+# Copyright (c) 2022-2024 Antmicro <www.antmicro.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+"""Helper functions for launch scripts construction."""
+
+from typing import Dict, List, Tuple
+
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, Shutdown
+from launch.conditions import LaunchConfigurationNotEquals
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+
+def common_parameters() -> (
+    Tuple[
+        List[DeclareLaunchArgument],
+        Dict[str, Tuple[LaunchConfiguration, DeclareLaunchArgument]],
+    ]
+):
+    """
+    Defines common parameters for CVNode launch scripts.
+
+    List of obligatory arguments:
+    - model_path - Path to the file to load model from
+    - measurements - Path where measurements will be stored
+    - report_path - Path to rendered inference report to store
+    - inference_configuration - Path to Kenning's JSON configuration file
+
+    List of optional arguments:
+    - scenario - Testing scenario strategy
+    - inference_timeout_ms - Inference timeout in milliseconds
+    - preserve_output - Whether manager should preserve current frame output
+    - publish_visualizations - Whether to publish data to visualize
+    - device - Device to inference model on
+    - log_level - Logging level
+
+    Returns
+    -------
+    List[DeclareLaunchArgument]
+        List of obligatory arguments.
+    Dict[str, Tuple[LaunchConfiguration, DeclareLaunchArgument]]
+        Dictionary of optional arguments.
+    """
+    # Obligatory arguments
+    obligatory = []
+    obligatory.append(
+        DeclareLaunchArgument(
+            "model_path",
+            description="Path to the file to load model from",
+        )
+    )
+    obligatory.append(
+        DeclareLaunchArgument(
+            "measurements",
+            description="Path where measurements will be stored",
+        )
+    )
+    obligatory.append(
+        DeclareLaunchArgument(
+            "report_path",
+            description="Path to rendered inference report to store",
+        )
+    )
+    obligatory.append(
+        DeclareLaunchArgument(
+            "inference_configuration",
+            description="Path to Kenning's JSON configuration file "
+            "with dataset, runtime and protocol specified",
+        ),
+    )
+
+    # Optional arguments
+    optional = {}
+
+    scenario = LaunchConfiguration("scenario", default="synthetic")
+    optional["scenario"] = (
+        scenario,
+        DeclareLaunchArgument(
+            "scenario",
+            default_value="synthetic",
+            description="Testing scenario strategy. Possible values:\n"
+            "synthetic - Manager is waiting for previous frame inference "
+            "to finish before sending next frame\n"
+            "real_world_last - Manager is sending frames with `inference_timeout_ms` latency, "  # noqa: E501
+            "but skip previous frame inference if next frame is ready\n"
+            "real_world_first - Manager is sending frames with `inference_timeout_ms` latency, "  # noqa: E501
+            "but skip frame if previous frame inference is not finished\n",
+            choices=["synthetic", "real_world_last", "real_world_first"],
+        ),
+    )
+
+    optional["inference_timeout_ms"] = (
+        LaunchConfiguration("inference_timeout_ms", default="300"),
+        DeclareLaunchArgument(
+            "inference_timeout_ms",
+            description="Inference timeout in milliseconds used by real_world_* scenarios",  # noqa: E501
+            default_value="300",
+        ),
+    )
+
+    optional["preserve_output"] = (
+        LaunchConfiguration("preserve_output", default="False"),
+        DeclareLaunchArgument(
+            "preserve_output",
+            default_value="False",
+            description="Indicates whether manager should preserve current "
+            "frame output. Useful for real_world_* scenarios when frame "
+            "inference is timeouted, so previous frame output is used",
+            choices=["True", "False"],
+        ),
+    )
+
+    optional["publish_visualizations"] = (
+        LaunchConfiguration("publish_visualizations", default="True"),
+        DeclareLaunchArgument(
+            "publish_visualizations",
+            description="Indicates whether to publish data for visualization "
+            "to ROS2 topic",
+            default_value="True",
+            choices=["True", "False"],
+        ),
+    )
+
+    optional["device"] = (
+        LaunchConfiguration("device", default="cuda"),
+        DeclareLaunchArgument(
+            "device",
+            description="Device to inference model on",
+            default_value="cuda",
+            choices=["cuda", "cpu"],
+        ),
+    )
+
+    optional["log_level"] = (
+        LaunchConfiguration("log_level", default="INFO"),
+        DeclareLaunchArgument(
+            "log_level",
+            description="Logging level",
+            default_value="INFO",
+            choices=["ERROR", "INFO", "DEBUG"],
+        ),
+    )
+    return obligatory, optional
+
+
+def cvnode_manager_node(
+    log_level: LaunchConfiguration,
+    visualizations: LaunchConfiguration,
+    timeout: LaunchConfiguration,
+    scenario: LaunchConfiguration,
+    preserve: LaunchConfiguration,
+    container_name: str = "cvnode_manager_container",
+    node_name: str = "cvnode_manager",
+) -> ComposableNodeContainer:
+    """
+    Composes node container for CVNodeManager.
+
+    Parameters
+    ----------
+    log_level : LaunchConfiguration
+        Logging level.
+    visualizations : LaunchConfiguration
+        Whether to publish data for visualization to ROS2 topic.
+    timeout : LaunchConfiguration
+        Inference timeout in milliseconds.
+    scenario : LaunchConfiguration
+        Testing scenario strategy.
+    preserve : LaunchConfiguration
+        Whether manager should preserve current frame output.
+    container_name : str, optional
+        Name of the container, by default "cvnode_manager_container".
+    node_name : str, optional
+        Name of the node, by default "cvnode_manager".
+
+    Returns
+    -------
+    ComposableNodeContainer
+        Composed node container.
+    """
+    return ComposableNodeContainer(
+        name=container_name,
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container",
+        composable_node_descriptions=[
+            ComposableNode(
+                package="cvnode_manager",
+                plugin="cvnode_manager::CVNodeManager",
+                name=node_name,
+                parameters=[
+                    {
+                        "publish_visualizations": visualizations,
+                        "inference_timeout_ms": timeout,
+                        "scenario": scenario,
+                        "preserve_output": preserve,
+                    }
+                ],
+            )
+        ],
+        output="both",
+        arguments=[
+            "--ros-args",
+            "--log-level",
+            log_level,
+        ],
+    )
+
+
+def kenning_test_report_node(
+    log_level: LaunchConfiguration, node_name: str = "kenning_node"
+) -> ExecuteProcess:
+    """
+    Creates a launch action for kenning test report node.
+
+    Parameters
+    ----------
+    log_level : LaunchConfiguration
+        Logging level.
+    node_name : str
+        Name of the node.
+
+    Returns
+    -------
+    ExecuteProcess
+        Launch action for kenning test report node.
+    """
+    return ExecuteProcess(
+        name=node_name,
+        cmd=[
+            [
+                "python -m kenning test report ",
+                "--json-cfg ",
+                LaunchConfiguration("inference_configuration"),
+                " --measurements ",
+                LaunchConfiguration("measurements"),
+                " --verbosity ",
+                log_level,
+                " --report-types detection ",
+                "--report-path ",
+                LaunchConfiguration("report_path"),
+                " --to-html",
+            ]
+        ],
+        on_exit=Shutdown(),
+        shell=True,
+    )
+
+
+def cvnode_manager_gui_node(
+    log_level: LaunchConfiguration,
+    container_name: str = "cvnode_manager_gui_container",
+    node_name: str = "cvnode_manager_gui",
+) -> ComposableNodeContainer:
+    """
+    Composes node container for CVNodeManagerGUI.
+
+    Parameters
+    ----------
+    log_level : LaunchConfiguration
+        Logging level.
+    container_name : str
+        Name of the container.
+    node_name : str
+        Name of the node.
+
+    Returns
+    -------
+    ComposableNodeContainer
+        Composed node container.
+    """
+    return ComposableNodeContainer(
+        name=container_name,
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container",
+        composable_node_descriptions=[
+            ComposableNode(
+                package="cvnode_manager",
+                plugin="cvnode_manager::gui::CVNodeManagerGUI",
+                name=node_name,
+            ),
+        ],
+        output="both",
+        arguments=[
+            "--ros-args",
+            "--log-level",
+            log_level,
+        ],
+        condition=LaunchConfigurationNotEquals(
+            "publish_visualizations", "False"
+        ),
+    )
--- examples/mask_rcnn/launch/mask_rcnn_onnx_launch.py
+++ examples/mask_rcnn/launch/mask_rcnn_onnx_launch.py
@@ -4,15 +4,16 @@
 """Launches MaskRCNN node with ONNXRuntime backend."""
 
 from launch import LaunchDescription
-from launch.actions import (
-    DeclareLaunchArgument,
-    ExecuteProcess,
-    SetEnvironmentVariable,
-    Shutdown,
-)
+from launch.actions import SetEnvironmentVariable
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import ComposableNodeContainer, Node
 from launch_ros.descriptions import ComposableNode
+
+from cvnode_base.utils.launch import (
+    common_parameters,
+    cvnode_manager_gui_node,
+    kenning_test_report_node,
+)
 
 
 def generate_launch_description() -> LaunchDescription:
@@ -24,111 +25,29 @@ def generate_launch_description() -> LaunchDescription:
     LaunchDescription
         Launch description with all nodes and parameters.
     """
-    args = []
+    args, optional_args = common_parameters()
 
-    # Obligatory arguments
-    args.append(
-        DeclareLaunchArgument(
-            "model_path",
-            description="Path to the file containing Mask R-CNN model in ONNX format",  # noqa: E501
-        )
-    )
-    args.append(
-        DeclareLaunchArgument(
-            "class_names_path",
-            description="Path to the file containing classes",
-        )
-    )
-    args.append(
-        DeclareLaunchArgument(
-            "measurements",
-            description="Path where measurements should be saved",
-        )
-    )
-    args.append(
-        DeclareLaunchArgument(
-            "report_path",
-            description="Path where rendered inference report should be saved",
-        )
+    # Arguments with default values
+    scenario, scenario_arg = optional_args["scenario"]
+    inference_timeout_ms, timeout_arg = optional_args["inference_timeout_ms"]
+    preserve_output, preserve_arg = optional_args["preserve_output"]
+    publish_visualizations, gui_arg = optional_args["publish_visualizations"]
+    device, device_arg = optional_args["device"]
+    log_level, log_level_arg = optional_args["log_level"]
+
+    args.extend(
+        [
+            scenario_arg,
+            timeout_arg,
+            preserve_arg,
+            gui_arg,
+            device_arg,
+            log_level_arg,
+        ]
     )
 
-    # Optional arguments
-    scenario = LaunchConfiguration("scenario", default="synthetic")
-    args.append(
-        DeclareLaunchArgument(
-            "scenario",
-            default_value="synthetic",
-            description="Testing scenario strategy. Possible values:\n"
-            "synthetic - Manager is waiting for previous frame inference "
-            "to finish before sending next frame\n"
-            "real_world_last - Manager is sending frames with `inference_timeout_ms` latency, "  # noqa: E501
-            "but skip previous frame inference if next frame is ready\n"
-            "real_world_first - Manager is sending frames with `inference_timeout_ms` latency, "  # noqa: E501
-            "but skip frame if previous frame inference is not finished\n",
-        )
-    )
-
-    inference_timeout_ms = LaunchConfiguration(
-        "inference_timeout_ms", default="300"
-    )
-    args.append(
-        DeclareLaunchArgument(
-            "inference_timeout_ms",
-            description="Inference timeout in milliseconds used by real_world_* scenarios",  # noqa: E501
-            default_value="300",
-        )
-    )
-
-    preserve_output = LaunchConfiguration("preserve_output", default="False")
-    args.append(
-        DeclareLaunchArgument(
-            "preserve_output",
-            default_value="False",
-            description="Indicates whether manager should preserve output current frame output."  # noqa: E501
-            "Useful for real_world_* scenarios when frame inference is timeouted "  # noqa: E501
-            " so previous frame output is used",
-        )
-    )
-
-    publish_visualizations = LaunchConfiguration(
-        "publish_visualizations", default="True"
-    )
-    args.append(
-        DeclareLaunchArgument(
-            "publish_visualizations",
-            description="Publish visualizations",
-            default_value="True",
-        )
-    )
-
-    device = LaunchConfiguration("device", default="cuda")
-    args.append(
-        DeclareLaunchArgument(
-            "device",
-            description="Device to execute model on",
-            default_value="cuda",
-        )
-    )
-
-    log_level = LaunchConfiguration("log_level", default="INFO")
-    args.append(
-        DeclareLaunchArgument(
-            "log_level", description="Log level", default_value="INFO"
-        )
-    )
-
-    config_json = LaunchConfiguration(
-        "inference_configuration",
-        default="./src/cvnode_base/examples/config/coco_inference.json",
-    )
-    args.append(
-        DeclareLaunchArgument(
-            "inference_configuration",
-            default_value="./src/cvnode_base/examples/config/coco_inference.json",  # noqa: E501
-            description="Path to Kenning's JSON configuration file "
-            "with dataset, runtime and protocol specified.",
-        )
-    )
+    kenning_node = kenning_test_report_node(log_level)
+    gui_node = cvnode_manager_gui_node()
 
     mask_rcnn_node = Node(
         package="cvnode_base",
@@ -137,8 +56,8 @@ def generate_launch_description() -> LaunchDescription:
         arguments=["--ros-args", "--log-level", log_level],
         parameters=[
             {
-                "class_names_path": LaunchConfiguration("class_names_path"),
                 "model_path": LaunchConfiguration("model_path"),
+                "class_names_path": LaunchConfiguration("class_names_path"),
                 "device": device,
             }
         ],
@@ -168,50 +87,14 @@ def generate_launch_description() -> LaunchDescription:
         arguments=["--ros-args", "--log-level", log_level],
     )
 
-    cvnode_manager_gui_node = ComposableNodeContainer(
-        name="cvnode_manager_gui_container",
-        namespace="",
-        package="rclcpp_components",
-        executable="component_container",
-        composable_node_descriptions=[
-            ComposableNode(
-                package="cvnode_manager",
-                plugin="cvnode_manager::gui::CVNodeManagerGUI",
-                name="cvnode_manager_gui",
-            ),
-        ],
-        output="both",
-    )
-
-    kenning_node = ExecuteProcess(
-        name="kenning_node",
-        cmd=[
-            [
-                "python -m kenning test report ",
-                "--json-cfg ",
-                config_json,
-                " --measurements ",
-                LaunchConfiguration("measurements"),
-                " --verbosity ",
-                log_level,
-                " --report-types detection ",
-                "--report-path ",
-                LaunchConfiguration("report_path"),
-                " --to-html",
-            ]
-        ],
-        on_exit=Shutdown(),
-        shell=True,
-    )
-
     return LaunchDescription(
         [
             SetEnvironmentVariable(
                 name="RCUTILS_CONSOLE_OUTPUT_FORMAT",
                 value="[{time}] [{severity}] [{name}] {function_name}: {message}",  # noqa: E501
             ),
-            cvnode_manager_gui_node,
             cvnode_manager_node,
+            gui_node,
             kenning_node,
             mask_rcnn_node,
             *args,
