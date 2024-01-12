f8604e5c207b8f431ad5b077e17ca8953da74ab0
Illia Vysochyn <ivysochyn@antmicro.com>
2024-01-12 19:31:00 +0100

    [#53508] examples: yolact: launch: Refactor launch script
    Signed-off-by: Illia Vysochyn <ivysochyn@antmicro.com>

--- examples/yolact/launch/yolact_kenning_launch.py
+++ examples/yolact/launch/yolact_kenning_launch.py
@@ -1,4 +1,4 @@
-# Copyright 2022-2023 Antmicro <www.antmicro.com>
+# Copyright 2022-2024 Antmicro <www.antmicro.com>
 #
 # SPDX-License-Identifier: Apache-2.0
 """Launches YOLACT CVNode with CVNodeManager and Kenning."""
@@ -6,13 +6,17 @@
 from launch import LaunchDescription
 from launch.actions import (
     DeclareLaunchArgument,
-    ExecuteProcess,
     SetEnvironmentVariable,
-    Shutdown,
 )
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import ComposableNodeContainer, Node
-from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import Node
+
+from cvnode_base.utils.launch_helper import (
+    common_parameters,
+    cvnode_manager_gui_node,
+    cvnode_manager_node,
+    kenning_test_report_node,
+)
 
 
 def generate_launch_description() -> LaunchDescription:
@@ -24,107 +28,35 @@ def generate_launch_description() -> LaunchDescription:
     LaunchDescription
         Launch description with all the nodes.
     """
-    args = []
+    args, optional_args = common_parameters()
+
     # Obligatory arguments
     args.append(
         DeclareLaunchArgument(
             "backend",
             description="Backend to execute YOLACT model on"
             "Possible values: tvm, onnxruntime, tflite",
-        )
-    )
-    args.append(
-        DeclareLaunchArgument(
-            "model_path", description="Path to the file containing model"
-        )
-    )
-    args.append(
-        DeclareLaunchArgument(
-            "measurements",
-            description="Path where measurements should be saved",
-        )
-    )
-    args.append(
-        DeclareLaunchArgument(
-            "report_path",
-            description="Path where rendered inference report should be saved",
+            choices=["tvm", "onnxruntime", "tflite"],
         )
     )
 
     # Optional arguments
-    scenario = LaunchConfiguration("scenario", default="synthetic")
-    args.append(
-        DeclareLaunchArgument(
-            "scenario",
-            default_value="synthetic",
-            description="Testing scenario strategy. Possible values:\n"
-            "synthetic - Manager is waiting for previous frame inference "
-            "to finish before sending next frame\n"
-            "real_world_last - Manager is sending frames with `inference_timeout_ms` latency, "  # noqa: E501
-            "but skip previous frame inference if next frame is ready\n"
-            "real_world_first - Manager is sending frames with `inference_timeout_ms` latency, "  # noqa: E501
-            "but skip frame if previous frame inference is not finished\n",
-        )
-    )
+    scenario, scenario_arg = optional_args["scenario"]
+    inference_timeout_ms, timeout_arg = optional_args["inference_timeout_ms"]
+    preserve_output, preserve_arg = optional_args["preserve_output"]
+    publish_visualizations, gui_arg = optional_args["publish_visualizations"]
+    device, device_arg = optional_args["device"]
+    log_level, log_level_arg = optional_args["log_level"]
 
-    inference_timeout_ms = LaunchConfiguration(
-        "inference_timeout_ms", default="300"
-    )
-    args.append(
-        DeclareLaunchArgument(
-            "inference_timeout_ms",
-            description="Inference timeout in milliseconds",
-            default_value="300",
-        )
-    )
-
-    preserve_output = LaunchConfiguration("preserve_output", default="False")
-    args.append(
-        DeclareLaunchArgument(
-            "preserve_output",
-            default_value="False",
-            description="Indicates whether manager should save output",
-        )
-    )
-
-    publish_visualizations = LaunchConfiguration(
-        "publish_visualizations", default="True"
-    )
-    args.append(
-        DeclareLaunchArgument(
-            "publish_visualizations",
-            description="Publish visualizations",
-            default_value="True",
-        )
-    )
-
-    device = LaunchConfiguration("device", default="cpu")
-    args.append(
-        DeclareLaunchArgument(
-            "device",
-            description="Device to execute TVM backend on",
-            default_value="cpu",
-        )
-    )
-
-    log_level = LaunchConfiguration("log_level", default="INFO")
-    args.append(
-        DeclareLaunchArgument(
-            "log_level", description="Log level", default_value="INFO"
-        )
-    )
-
-    config_json = LaunchConfiguration(
-        "inference_configuration",
-        default="./src/cvnode_base/examples/config/lindenthal_camera_traps_inference.json",  # noqa: E501
-    )
-    args.append(
-        DeclareLaunchArgument(
-            "inference_configuration",
-            default_value="./src/cvnode_base/examples/config/lindenthal_camera_traps_inference.json",  # noqa: E501
-            description="Path to Kenning's JSON configuration file "
-            "with dataset, runtime and protocol specified.",
-        )
+    args.extend(
+        [
+            scenario_arg,
+            timeout_arg,
+            preserve_arg,
+            gui_arg,
+            device_arg,
+            log_level_arg,
+        ]
     )
 
     # Nodes
@@ -142,64 +74,14 @@ def generate_launch_description() -> LaunchDescription:
         ],
     )
 
-    cvnode_manager_node = ComposableNodeContainer(
-        name="cvnode_manager_container",
-        namespace="",
-        package="rclcpp_components",
-        executable="component_container",
-        composable_node_descriptions=[
-            ComposableNode(
-                package="cvnode_manager",
-                plugin="cvnode_manager::CVNodeManager",
-                name="sample_cvnode_manager",
-                parameters=[
-                    {
-                        "publish_visualizations": publish_visualizations,
-                        "inference_timeout_ms": inference_timeout_ms,
-                        "scenario": scenario,
-                        "preserve_output": preserve_output,
-                    }
-                ],
-            )
-        ],
-        output="both",
-        arguments=["--ros-args", "--log-level", log_level],
-    )
-
-    cvnode_manager_gui_node = ComposableNodeContainer(
-        name="cvnode_manager_gui_container",
-        namespace="",
-        package="rclcpp_components",
-        executable="component_container",
-        composable_node_descriptions=[
-            ComposableNode(
-                package="cvnode_manager",
-                plugin="cvnode_manager::gui::CVNodeManagerGUI",
-                name="cvnode_manager_gui",
-            ),
-        ],
-        output="both",
-    )
-
-    kenning_node = ExecuteProcess(
-        name="kenning_node",
-        cmd=[
-            [
-                "python -m kenning test report ",
-                "--json-cfg ",
-                config_json,
-                " --measurements ",
-                LaunchConfiguration("measurements"),
-                " --verbosity ",
-                log_level,
-                " --report-types detection ",
-                "--report-path ",
-                LaunchConfiguration("report_path"),
-                " --to-html",
-            ]
-        ],
-        on_exit=Shutdown(),
-        shell=True,
+    kenning_node = kenning_test_report_node(log_level)
+    gui_node = cvnode_manager_gui_node(log_level)
+    manager_node = cvnode_manager_node(
+        log_level=log_level,
+        visualizations=publish_visualizations,
+        timeout=inference_timeout_ms,
+        preserve=preserve_output,
+        scenario=scenario,
     )
 
     return LaunchDescription(
@@ -208,9 +90,9 @@ def generate_launch_description() -> LaunchDescription:
                 name="RCUTILS_CONSOLE_OUTPUT_FORMAT",
                 value="[{time}] [{severity}] [{name}] {function_name}: {message}",  # noqa: E501
             ),
-            cvnode_manager_gui_node,
-            cvnode_manager_node,
+            gui_node,
             kenning_node,
+            manager_node,
             yolact_node,
             *args,
         ]
